c5c39f7b77e43302812707b48d2bb52f46725b8d
adorechic <adorechic@gmail.com>
2015-12-17 21:11:14 +0900

    Test

--- app/queue_handler.rb
+++ app/queue_handler.rb
@@ -1,6 +1,10 @@
 class QueueHandler < PerfectQueue::Application::Base
   def run
-    puts "task #{task.inspect}"
+    puts "!!task #{task.inspect}"
+    5.times do |i|
+      puts i
+      sleep 1
+    end
     task.finish!
   end
 end
